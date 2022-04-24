6805eef94d932d215435ad7f8f5589cd562c24ec
WeiTing Chen <happyalien543@gmail.com>
2022-04-24 23:51:10 +0800

    20220424-test

--- src/test.js
+++ src/test.js
@@ -11,6 +11,7 @@ const Test = () => {
     const { id } = useParams();
     const linkref = useRef(id);
     const [scroll, setScroll] = useState(0);
+    const canvasref = useRef();
 
 
     const obj = { name: 'Tom', age: 18 };
@@ -20,12 +21,14 @@ const Test = () => {
     //把urlencoding轉為物件
     // console.log(qs.parse(str));
 
+
     //radio操縱
-    const [ra, setRa] = useState('');
-    const [ra2, setRa2] = useState('');
-    const [ra3, setRa3] = useState('');
-    const [allra, setAllra] = useState('');
-    const [rank, setRank] = useState([]);
+    const [ra, setRa] = useState('');//第一項radio監聽
+    const [ra2, setRa2] = useState('');//第二項radio監聽
+    const [ra3, setRa3] = useState('');//第三項radio監聽
+    const [allra, setAllra] = useState('');//三組radio value整合傳入
+    const [rank, setRank] = useState([]);//存入過濾後的rankdata
+    const rankdata = [{ id: 1, title: '電子報' }, { id: 2, title: '工作坊' }, { id: 3, title: '教育訓練' }];
 
 
     const formik = useFormik({
@@ -83,10 +86,20 @@ const Test = () => {
             console.log(err)
         }
     }
+    useEffect(() => {
+        fetch()
+    }, []);
 
 
     useEffect(() => {
-        fetch()
+        const context = canvasref.current.getContext('2d');
+        context.moveTo(100, 100);
+        context.lineTo(200, 100);
+        context.lineTo(150, 150);
+        context.lineTo(100, 100);
+        context.fillStyle = 'rgb(122,122,122)'
+        // context.stroke();
+        context.fill();
     }, []);
 
 
@@ -95,6 +108,8 @@ const Test = () => {
     //         formik.handleSubmit
     //     }
     // }
+
+
 
     //判斷useParams傳入的id是否正確，正確的話視窗就往ref的位置跑
     useEffect(() => {
@@ -105,25 +120,42 @@ const Test = () => {
     }, [scroll]);
 
 
+
+    // radio動態測試
     useEffect(() => {
         setRank([]);
         setAllra([ra, ra2, ra3])
     }, [ra, ra2, ra3]);
     useEffect(() => {
+        // 先判斷三組radio value是否都有值
         if (ra !== '' && ra2 !== '' && ra3 !== '') {
+            // switch (allra.toString()) {
+            //     case 'ra1,ra3,ra5': return (setRank([{ id: 1, title: '電子報' }, { id: 2, title: '工作坊' }, { id: 3, title: '教育訓練' }]))
+            //     case 'ra1,ra3,ra6': return (setRank([{ id: 1, title: '電子報' }, { id: 3, title: '教育訓練' }]))
+            //     case 'ra1,ra4,ra5': return (setRank([{ id: 2, title: '工作坊' }, { id: 3, title: '教育訓練' }]))
+            //     case 'ra1,ra4,ra6': return (setRank([{ id: 1, title: '電子報' }, { id: 2, title: '工作坊' }]))
+            //     case 'ra2,ra3,ra5': return (setRank([{ id: 3, title: '教育訓練' }]))
+            //     case 'ra2,ra3,ra6': return (setRank([{ id: 1, title: '電子報' }]))
+            //     case 'ra2,ra4,ra5': return (setRank([{ id: 2, title: '工作坊' }]))
+            //     case 'ra2,ra4,ra6': return (setRank([{ id: 1, title: '電子報' }, { id: 2, title: '工作坊' }]))
+            // }
+
+            //因為傳入的value為自串，所以整合value必須從陣列再次轉字串
             switch (allra.toString()) {
-                case 'ra1,ra3,ra5': return (setRank([{ id: 1, title: '電子報' }, { id: 2, title: '工作坊' }, { id: 3, title: '教育訓練' }]))
-                case 'ra1,ra3,ra6': return (setRank([{ id: 1, title: '電子報' }, { id: 3, title: '教育訓練' }]))
-                case 'ra1,ra4,ra5': return (setRank([{ id: 2, title: '工作坊' }, { id: 3, title: '教育訓練' }]))
-                case 'ra1,ra4,ra6': return (setRank([{ id: 1, title: '電子報' }, { id: 2, title: '工作坊' }]))
-                case 'ra2,ra3,ra5': return (setRank([{ id: 3, title: '教育訓練' }]))
-                case 'ra2,ra3,ra6': return (setRank([{ id: 1, title: '電子報' }]))
-                case 'ra2,ra4,ra5': return (setRank([{ id: 2, title: '工作坊' }]))
-                case 'ra2,ra4,ra6': return (setRank([{ id: 1, title: '電子報' }, { id: 2, title: '工作坊' }]))
+                //過濾rankdata
+                case 'ra1,ra3,ra5': return (setRank(rankdata.filter(c => c.id < 3)))
+                case 'ra1,ra3,ra6': return (setRank(rankdata.filter(c => c.id === 1 || c.id === 3)))
+                case 'ra1,ra4,ra5': return (setRank(rankdata.filter(c => c.id === 2 || c.id === 3)))
+                case 'ra1,ra4,ra6': return (setRank(rankdata.filter(c => c.id === 1 || c.id === 2)))
+                case 'ra2,ra3,ra5': return (setRank(rankdata.filter(c => c.id === 3)))
+                case 'ra2,ra3,ra6': return (setRank(rankdata.filter(c => c.id === 1)))
+                case 'ra2,ra4,ra5': return (setRank(rankdata.filter(c => c.id === 2)))
+                case 'ra2,ra4,ra6': return (setRank(rankdata.filter(c => c.id === 1 || c.id === 2 || c.id === 3)))
             }
         }
-        console.log('render')
+        // console.log('render')
     }, [allra]);
+
 
     return (
         <div className={style.aaa} style={{ backgroundImage: 'url(/img/bk.jpg)' }}>
@@ -164,6 +196,7 @@ const Test = () => {
                             <input type="text" value={list} onChange={(e) => { handletodo(e) }} />
                             <button type='button' onClick={() => { handleSubmit(list) }}>送出</button>
                         </div>
+                        <p style={{ paddingLeft: '25px' }}>{new Date().toLocaleTimeString()}</p>
                         <div className={style.contWrap}>
                             <ul className={style.todoUl}>
                                 {content.map(cont => (
@@ -177,7 +210,7 @@ const Test = () => {
             </div>
 
 
-
+            {/* radio動態測試 */}
             <div className={style.radiotest}>
                 <div className={style.ra1}>
                     <input type="radio" name="" id="" value='ra1'
@@ -218,9 +251,18 @@ const Test = () => {
 
 
 
-            <div className='aa' style={{ height: '800px', backgroundColor: '#fde' }}></div>
+            <div className='aa' style={{ height: '800px' }}>
+                <canvas id='canvas' width='500' height='300' style={{ display: 'block', border: '1px solid #5a5a5a', margin: '0 auto' }} ref={canvasref}></canvas>
+            </div>
             <div className='bb' style={{ height: '800px', backgroundColor: '#fda' }} ref={linkref}></div>
-            <div className='cc' style={{ height: '800px', backgroundColor: '#fce' }}></div>
+            <div className={style.cc} style={{ height: '800px' }}>
+                <div className={style.roundWrap}>
+                    <div className={style.round}></div>
+                    <div className={style.round2}></div>
+                    <div className={style.round3}></div>
+                </div>
+
+            </div>
 
         </div>
     )
